c3d924733a925aae456989aa0f05a3bf4a615a87
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-02-21 17:27:27 +0100

    closures: challenge 17

--- 02_closures.js
+++ 02_closures.js
@@ -340,18 +340,27 @@ function average() {
 /******************************************************************************/
 
 // CHALLENGE 17
-function makeFuncTester(arrOfTests) {}
+function makeFuncTester(arrOfTests) {
+  function inner(callback) {
+    for (const subArr of arrOfTests) {
+      return callback(subArr[0]) === subArr[1] ? true : false;
+    }
+  }
+  return inner;
+}
 
 // /*** Uncomment these to check your work! ***/
 // const capLastTestCases = [];
-// capLastTestCases.push(['hello', 'hellO']);
-// capLastTestCases.push(['goodbye', 'goodbyE']);
-// capLastTestCases.push(['howdy', 'howdY']);
+// capLastTestCases.push(["hello", "hellO"]);
+// capLastTestCases.push(["goodbye", "goodbyE"]);
+// capLastTestCases.push(["howdy", "howdY"]);
 // const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
-// const capLastAttempt1 = str => str.toUpperCase();
-// const capLastAttempt2 = str => str.slice(0, -1) + str.slice(-1).toUpperCase();
+// const capLastAttempt1 = (str) => str.toUpperCase();
+// const capLastAttempt2 = (str) => str.slice(0, -1) + str.slice(-1).toUpperCase();
 // console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
 // console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
+
+/******************************************************************************/
 
 // CHALLENGE 18
 function makeHistory(limit) {}
