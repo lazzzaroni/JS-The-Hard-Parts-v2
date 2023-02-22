32dea0663994d88e179f9b17bb4b8e96d308cfcc
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-02-22 13:21:28 +0100

    asynchronicity: challenge 3

--- 03_asynchronicity.js
+++ 03_asynchronicity.js
@@ -30,10 +30,15 @@ function delayedGreet() {
 /* CHALLENGE 3 */
 
 function helloGoodbye() {
-  // ADD CODE HERE
+  setTimeout(() => {
+    console.log("good bye");
+  }, 3000);
+  console.log("hello");
 }
 // Uncomment the following line to check your work!
 // helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+
+/******************************************************************************/
 
 /* CHALLENGE 4 */
 
