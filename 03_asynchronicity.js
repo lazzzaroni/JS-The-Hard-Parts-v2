c6b762ea5e144ce6fddac5c4c8018a8ea8983c1f
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-02-22 14:45:15 +0100

    asynchronicity: challenge 8

--- 03_asynchronicity.js
+++ 03_asynchronicity.js
@@ -115,13 +115,19 @@ function delayCounter(target, wait) {
 /* CHALLENGE 8 */
 
 function promised(val) {
-  // ADD CODE HERE
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(val);
+    }, 2000);
+  });
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const createPromise = promised('wait for it...');
+// const createPromise = promised("wait for it...");
 // createPromise.then((val) => console.log(val));
 // will log "wait for it..." to the console after 2 seconds
+
+/******************************************************************************/
 
 /* CHALLENGE 9 */
 
