ec7682d3c3bbb41ba158e1f4bb6d56bb57510765
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-02-24 14:13:21 +0100

    oop: challenge Mixins

--- 04_oop.js
+++ 04_oop.js
@@ -170,6 +170,7 @@ function adminFactory(name, score) {
 }
 
 /*** CHALLENGE 14 ***/
+
 // only admins have an access to this method
 adminFunctionStore.sharePublicMessage = function () {
   console.log("Welcome users!");
@@ -200,13 +201,13 @@ class Dog {
 const robotMixin = {
   skin: "metal",
   speak: function () {
-    console.log(`I have ${this.legs} legs and am made of ${this.skin}`);
+    console.log(`I have ${this.legs} legs and I am made of ${this.skin}!`);
   },
 };
 
 let robotFido = new Dog();
 
-// robotFido = /* Put code here to give Fido robot skills */;
+robotFido = Object.assign(robotFido, robotMixin);
 
 // /********* Uncomment to test your work! *********/
-// robotFido.speak() // -> Logs "I am made of metal"
+// robotFido.speak(); // -> Logs "I have 4 legs and I am made of metal"
